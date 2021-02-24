84e861b040a992188f7b172861b59ef9aafceda1
mnlima <nasc.mlima@gmail.com>
2021-02-24 18:37:57 -0300

    Refatoração e testes unitários

---
+++ src/__tests__/First.test.ts
@@ -0,0 +1,8 @@
+
+
+describe("First", () => {
+
+  it("deve ser possivel somar dois numeros", () => {
+    expect(2+2).toBe(4)
+  })
+})
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,20 +1,19 @@
-import { request, Request, response, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
-
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repositories/UsersRepository';
 class UserController {
 
   async create(req: Request, res: Response) {
     const { name, email } = req.body;
 
-    const usersRepository = getRepository(User);
+    const usersRepository = getCustomRepository(UserRepository);
 
-    const userAlreadyExists = await usersRepository.findOne({ 
-      email 
+    const userAlreadyExists = await usersRepository.findOne({
+      email
     });
 
-    if(userAlreadyExists) {
-      return res.status(400).json({ 
+    if (userAlreadyExists) {
+      return res.status(400).json({
         error: "User already exists"
       });
     }
